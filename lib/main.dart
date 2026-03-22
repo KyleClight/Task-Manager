a6ebb13b66336882e155166070123a4c95589c09
KyleClight <belkius102@gmail.com>
2026-03-22 20:07:11 +0600

    add: added text field processing, recording information in expanded using ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,70 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Запуск древа FlutterUI
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(),
-      title: 'Task Manager',
-      theme: ThemeData(
-        
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-    );
+      title: "Task Manager",
+      home: const TaskPage(),
+      );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class TaskPage extends StatefulWidget {
+  const TaskPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TaskPage> createState() => _TaskPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
-
+class _TaskPageState extends State<TaskPage> {
+  // controller, который управляет TextField (TextInput). Внутри него имеются методы, позволяющие читать, изменять и записывать информацию
+  TextEditingController controller = TextEditingController(); 
+  List<String> tasks = []; // Общий пул задач, которые попадают туда через $TextField
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Task Manager"),
-      ),
-      body: Center(
-        child: Text("Nothing to do")
+      body: Column(children: [
+        SafeArea(child: Padding( // child: какой виджет должен находиться внутри штор
+          padding:EdgeInsetsGeometry.all(0))
+          ),
+        Row(children: [
+          Expanded(child: TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: 'Enter your task',
+          ),
+        )), 
+        ElevatedButton(onPressed: () {
+          setState(() {
+            tasks.add(controller.text);
+            controller.clear();
+          });
+        }, child: Text('Add')),
+        ]),
+        Expanded(child: ListView( // "Займи всё оставшееся место на экране". Без Height
+            children: tasks.asMap().entries.map((entry) { // asMap - получение пары key: Value;
+              int index = entry.key;
+              String task = entry.value;
+
+              return Dismissible(key: ValueKey(task),
+              onDismissed: (direction) {
+                setState(() {
+                  tasks.removeAt(index);
+                });
+              }, 
+              child: ListTile(title: Text(task),));
+            }).toList(),
+        )
+        )
+      ],
       ),
     );
   }
