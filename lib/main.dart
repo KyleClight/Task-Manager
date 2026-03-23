ced7590ab5f8af04a2fd0b6996df876e84dd4301
KyleClight <belkius102@gmail.com>
2026-03-23 20:23:25 +0600

    add: stateful local storage saving tasks; minor updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp()); // Запуск древа FlutterUI
@@ -9,10 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Task Manager",
-      home: const TaskPage(),
-      );
+    return MaterialApp(title: "Task Manager", home: const TaskPage());
   }
 }
 
@@ -25,46 +23,86 @@ class TaskPage extends StatefulWidget {
 
 class _TaskPageState extends State<TaskPage> {
   // controller, который управляет TextField (TextInput). Внутри него имеются методы, позволяющие читать, изменять и записывать информацию
-  TextEditingController controller = TextEditingController(); 
-  List<String> tasks = []; // Общий пул задач, которые попадают туда через $TextField
-  
+  TextEditingController controller = TextEditingController();
+  List<String> tasks =
+      []; // Общий пул задач, которые попадают туда через $TextField
+
+  @override
+  void initState() {
+    // Состояние, вызываемое при первой компиляции приложения
+    super.initState();
+    loadTasks();
+  }
+
+  Future<void> loadTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final loadedTasks = prefs.getStringList('tasks') ?? [];
+    tasks = loadedTasks;
+    setState(() {
+      tasks = loadedTasks;
+    });
+  }
+
+  Future<void> _saveTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('tasks', tasks);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        SafeArea(child: Padding( // child: какой виджет должен находиться внутри штор
-          padding:EdgeInsetsGeometry.all(0))
+      body: Column(
+        children: [
+          SafeArea(
+            child: Padding(
+              // child: какой виджет должен находиться внутри штор
+              padding: EdgeInsetsGeometry.all(0),
+            ),
           ),
-        Row(children: [
-          Expanded(child: TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            hintText: 'Enter your task',
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(hintText: 'Enter your task'),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (controller.text.isNotEmpty) {
+                    setState(() {
+                      tasks.add(controller.text);
+                      controller.clear();
+                    });
+                    _saveTasks();
+                  }
+                },
+                child: Text('Add'),
+              ),
+            ],
           ),
-        )), 
-        ElevatedButton(onPressed: () {
-          setState(() {
-            tasks.add(controller.text);
-            controller.clear();
-          });
-        }, child: Text('Add')),
-        ]),
-        Expanded(child: ListView( // "Займи всё оставшееся место на экране". Без Height
-            children: tasks.asMap().entries.map((entry) { // asMap - получение пары key: Value;
-              int index = entry.key;
-              String task = entry.value;
+          Expanded(
+            child: ListView(
+              // "Займи всё оставшееся место на экране". Без Height
+              children: tasks.asMap().entries.map((entry) {
+                // asMap - получение пары key: Value;
+                int index = entry.key;
+                String task = entry.value;
 
-              return Dismissible(key: ValueKey(task),
-              onDismissed: (direction) {
-                setState(() {
-                  tasks.removeAt(index);
-                });
-              }, 
-              child: ListTile(title: Text(task),));
-            }).toList(),
-        )
-        )
-      ],
+                return Dismissible(
+                  key: ValueKey(task),
+                  onDismissed: (direction) {
+                    setState(() {
+                      tasks.removeAt(index);
+                    });
+                    _saveTasks();
+                  },
+                  child: ListTile(title: Text(task)),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
