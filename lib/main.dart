72449b2d8d8886d1771f7ecda79ad4fcdf5a8df5
KyleClight <belkius102@gmail.com>
2026-03-23 21:59:59 +0600

    add: checkbox-status, tasks был String, теперь Object() со значениями isDone и title; minor refactor

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() {
   runApp(const MyApp()); // Запуск древа FlutterUI
 }
 
+class Task {
+  String title;
+  bool isDone;
+
+  Task({required this.title, this.isDone = false});
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -24,30 +31,36 @@ class TaskPage extends StatefulWidget {
 class _TaskPageState extends State<TaskPage> {
   // controller, который управляет TextField (TextInput). Внутри него имеются методы, позволяющие читать, изменять и записывать информацию
   TextEditingController controller = TextEditingController();
-  List<String> tasks =
-      []; // Общий пул задач, которые попадают туда через $TextField
+  List<Task> tasks = []; // Общий пул задач, которые попадают туда через $TextField
 
   @override
   void initState() {
     // Состояние, вызываемое при первой компиляции приложения
     super.initState();
-    loadTasks();
+    // loadTasks();
   }
+  
+  // Future<void> loadTasks() async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   final loadedTasks = prefs.getStringList('tasks') ?? [];
+  //   tasks = loadedTasks;
+  //   setState(() {
+  //     tasks = loadedTasks;
+  //   });
+  // }
 
-  Future<void> loadTasks() async {
-    final prefs = await SharedPreferences.getInstance();
-    final loadedTasks = prefs.getStringList('tasks') ?? [];
-    tasks = loadedTasks;
+  // Future<void> _saveTasks() async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   await prefs.setStringList('tasks', tasks);
+  // }
+
+  void _deleteTask(int index) {
     setState(() {
-      tasks = loadedTasks;
+      tasks.removeAt(index);
     });
+    // _saveTasks();
   }
-
-  Future<void> _saveTasks() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList('tasks', tasks);
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,10 +84,10 @@ class _TaskPageState extends State<TaskPage> {
                 onPressed: () {
                   if (controller.text.isNotEmpty) {
                     setState(() {
-                      tasks.add(controller.text);
+                      tasks.add(Task(title: controller.text));
                       controller.clear();
                     });
-                    _saveTasks();
+                    // _saveTasks();
                   }
                 },
                 child: Text('Add'),
@@ -87,17 +100,24 @@ class _TaskPageState extends State<TaskPage> {
               children: tasks.asMap().entries.map((entry) {
                 // asMap - получение пары key: Value;
                 int index = entry.key;
-                String task = entry.value;
-
-                return Dismissible(
-                  key: ValueKey(task),
-                  onDismissed: (direction) {
+                String task = entry.value.title;
+                bool isDone = entry.value.isDone;
+                
+                return ListTile(
+                  leading: Checkbox(value: isDone, onChanged: (bool? newValue) {
                     setState(() {
-                      tasks.removeAt(index);
+                      tasks[index].isDone = newValue ?? false;
                     });
-                    _saveTasks();
-                  },
-                  child: ListTile(title: Text(task)),
+                  }),
+                  title: Text(
+                    task,
+                    style: TextStyle(
+                      color: isDone ? Colors.grey : const Color.fromARGB(255, 11, 11, 11),
+                      decoration: isDone ? TextDecoration.lineThrough : TextDecoration.none,
+                    )
+                  ),
+                  trailing: IconButton(onPressed: () => _deleteTask(index),
+                  icon: Icon(Icons.delete, color: Colors.redAccent)),
                 );
               }).toList(),
             ),
